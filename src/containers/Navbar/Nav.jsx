14d63f55a8f5f7caf0d617372060885ea8bccf5b
is-Lew <lewis.halder@outlook.com>
2022-02-09 21:57:27 +0000

    added nav bar checkboxes

--- src/containers/Navbar/Nav.jsx
+++ src/containers/Navbar/Nav.jsx
@@ -5,6 +5,19 @@ function Nav() {
   return (
     <div className='navContainer'>
       <input className="navContainer__input" placeholder="Search..." type="text" />
+      <div className="navContainer__checkInput">
+      <label htmlFor="abv">{`HIGH ABV(> 6%)`}</label>
+      <input className="navContainer___checkbox" type="checkbox" id="abv" name="abv" />
+      </div>
+      <div className="navContainer__checkInput">
+      <label htmlFor="range">{`Classic Range`}</label>
+      <input className="navContainer___checkbox" type="checkbox" id="range" name="range" />
+      </div>
+      <div className="navContainer__checkInput">
+      <label htmlFor="ph">{`Acidic (ph < 4)`}</label>
+      <input className="navContainer___checkbox" type="checkbox" id="ph" name="ph" />
+      </div>
+      
     </div>
   )
 }
