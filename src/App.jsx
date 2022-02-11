e2fc8ae43414f5508971b63ccbeb2c9fc074a518
is-Lew <lewis.halder@outlook.com>
2022-02-11 16:49:12 +0000

    added paging, styling not done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Main from "./containers/Main/Main";
 import Nav from "./containers/Navbar/Nav";
 import menu from "./Assets/Images/menu-icon.png";
 
+
 const App = ()  => {
   useEffect(() => {
     fetch("https://api.punkapi.com/v2/beers")
@@ -21,8 +22,13 @@ const App = ()  => {
   const [ph, setPh] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
   const [beers, setBeers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); 
+
   const beersArr = beers;
-  
+  const beersPerPage = 6
+  const indexOfLastBeer = currentPage * beersPerPage;
+  const indexOfFirstBeer = indexOfLastBeer - beersPerPage;
+ 
 
   const handleInput = (event) => {
     const cleanInput = event.target.value.toLowerCase();
@@ -42,6 +48,10 @@ const App = ()  => {
     setRange(!range);
   };
 
+  const handlePage = (pageNumber) => {
+    setCurrentPage(pageNumber)
+  }
+
   const searchResults = beersArr.filter((beer) => {
     return beer.name.toLowerCase().includes(searchTerm);
   });
@@ -57,6 +67,10 @@ const App = ()  => {
       return beer.first_brewed.split("/")[1] <= 2011;
     }
   });
+
+  const currentBeers = filteredBeers.slice(indexOfFirstBeer, indexOfLastBeer);
+  
+
 
   return (
     <div className="container">
@@ -82,7 +96,8 @@ const App = ()  => {
       ) : (
         ""
       )}
-      <Main beersArr={filteredBeers} />
+      <Main beersArr={currentBeers} beersPerPage={beersPerPage} totalBeers={beers.length} handlePage={handlePage} />
+      
     </div>
   );
 }
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import CardList from '../../components/CardList/CardList'
 import "./Main.scss"
+import Pagination from "../../components/Pagination/Pagination";
 
 
 const Main = props => {
-    const {beersArr} = props;
+    const {beersArr, beersPerPage, totalBeers, handlePage} = props;
   return (
     <div className='mainContainer'>
-    <CardList beersArr = {beersArr}/>    
+    <CardList beersArr = {beersArr}/> 
+    <Pagination  beersPerPage={beersPerPage} totalBeers={totalBeers} handlePage={handlePage}/>   
     </div>
   )
 }
